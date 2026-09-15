d9f4212ab59058780d688c5d8fb99beb52b68e57
DevelSajjad <sinigdho01@gmail.com>
2026-09-16 01:37:01 +0600

    mysql connection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+
+from app.database.connection import engine
+from app.database.base import Base
+from app.models import User
+
 from app.api.routes.users import router as user_routes
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
