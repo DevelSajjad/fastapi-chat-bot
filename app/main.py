8796f4c68afdfb4550d8888732218151431e500a
DevelSajjad <sinigdho01@gmail.com>
2026-09-16 01:45:48 +0600

    free commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,5 @@
 from fastapi import FastAPI
-
-from app.database.connection import engine
-from app.database.base import Base
-from app.models import User
-
 from app.api.routes.users import router as user_routes
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
